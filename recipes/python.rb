847d6b1745a957bd24f217b53cb1e5d4420a13ed
Bao Nguyen <bao@fastly.com>
2015-06-09 14:36:16 -0700

    Adding default build system for python and fpm.

---
+++ recipes/python.rb
@@ -0,0 +1,12 @@
+include_recipe "apt"
+%w(autogen git rsync tree s3cmd python-setuptools rubygems ruby-1.8-dev python-dev).each do |i|
+  package i
+end
+
+execute "fpm" do
+  command "gem install fpm"
+end
+
+execute "virtualenv" do
+  command "easy_install virtualenv virtualenv-tools"
+end
