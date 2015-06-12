52100809c5eb47946c687e1f8819b8ee612ea347
Bao Nguyen <bao@fastly.com>
2015-06-12 11:39:10 -0700

    Support databag in kitchen.
    Correcting syntax error.

--- recipes/pbuilder.rb
+++ recipes/pbuilder.rb
@@ -3,6 +3,6 @@
 end
 
 execute "pbuilder" do
-  command "export DIST=#{node['build']['dist']} /usr/sbin/pbuilder create"
+  command "DIST=#{node['build']['dist']} /usr/sbin/pbuilder create"
   not_if { ::File.exists?("/var/cache/pbuilder/base.tgz")}
 end
