3c0a1b83574140836e31cee19dd4c2ed7cb1aaa5
Bao Nguyen <bao@fastly.com>
2015-06-17 14:56:04 -0700

    Making use of rbenv cookbook to manage ruby version

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ source 'https://supermarket.getchef.com'
 metadata
 
 cookbook 'apt'
+cookbook 'rbenv', git: 'git://github.com/RiotGamesCookbooks/rbenv-cookbook'
--- recipes/omnibus.rb
+++ recipes/omnibus.rb
@@ -1,8 +1,14 @@
 include_recipe "apt"
-%w(autogen git rsync tree s3cmd python-setuptools rubygems ruby1.8-dev python-dev).each do |i|
+%w(autogen git rsync tree s3cmd).each do |i|
   package i
 end
 
-execute "bundler" do
-  command "gem install bundler"
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+
+include_recipe "rbenv::rbenv_vars"
+rbenv_ruby "1.9.3-p194"
+
+rbenv_gem "bundler" do
+  ruby_version "1.9.3-p194"
 end
