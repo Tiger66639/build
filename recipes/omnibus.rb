bd5edb4a20659e601a4b1c7b6caab5df830ae1ee
Bao Nguyen <bao@fastly.com>
2015-06-17 15:09:40 -0700

    Marking this version as global/system wide.

--- recipes/omnibus.rb
+++ recipes/omnibus.rb
@@ -7,7 +7,9 @@ include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
 include_recipe "rbenv::rbenv_vars"
-rbenv_ruby "1.9.3-p194"
+rbenv_ruby "1.9.3-p194" do
+  global true
+end
 
 rbenv_gem "bundler" do
   ruby_version "1.9.3-p194"
